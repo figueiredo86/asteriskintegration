7bc0d111e4faa1cb82cfddb69775cc1347f582a6
root <root@stfalpc000000-w.stefanini.dom>
2015-01-19 12:43:09 -0200

    Ajustes no arquivo de conexão com o AD

--- reset_user_pwd.pl
+++ reset_user_pwd.pl
@@ -1,48 +1,56 @@
 #!/usr/bin/perl -w
-#
-# changing user passwords in AD
-#
+
 use strict;
 use Net::LDAP;
-use Asterisk::AGI;
+#use Asterisk::AGI;
 use Config::Tiny;
-
-
-# module needed to encode AD password
 use Unicode::String qw(utf8);
-#
-# ARGV is username password
+
+my $confile = Config::Tiny->new;
+
+$confile = Config::Tiny->read('/home/asteriskintegration/properties.ini');
+$adadminuser = $confile->{ad}->{adadminuser};
+$adadminpasswd = $confile->{ad}->{adadminpasswd};
+$adhost = $confile->{ad}->{adhost};
+
+
 my $username = $ARGV[0];
 my $passwd = $ARGV[1];
  
-my $adsvr='dc1.example.net';
-my $adbinddn='cn=administrator,cn=users,dc=example,dc=net';
-my $adpw='youradministratorpassword';
+my $adsvr='';
+my $adbinddn="cn=$adadminuser,cn=,dc=stefanini,dc=dom";
+my $adpw=$adadminpasswd;
  
+
 # Connect to the AD server
-my $ad=Net::LDAP->new($adsvr,
-                      version => 3,
-                      scheme  => 'ldaps',
-                      port    => 636,
-                     )
-  or die "can't connect to $adsvr: $@";
+my $ad=Net::LDAP->new(
+ $adsvr,
+ version => 3,
+ scheme  => 'ldaps',
+ port    => 636,
+) or die "can't connect to $adsvr: $@";
+
 # Bind as Administrator
 $result=$ad->bind($adbinddn, password=>$adpw);
+
 if ($result->code) {
-  LDAPerror ("binding",$result);
-  exit 1;
+ LDAPerror ("binding",$result);
+ exit 1;
 };
  
 # check for username, get DN
 $result = $ad->search(
-                       base   => "cn=users,dc=example,dc=net",
-                       filter => "(samAccountName=$username)",
-                       attrs  => ['distinguishedName']
-                       );
+ base   => "cn=users,dc=stefanini,dc=dom",
+ filter => "(samAccountName=$username)",
+ attrs  => ['distinguishedName']
+);
 $result->code && die $result->error;
-if ($result->entries != 1 ) { die "ERROR: User not found in AD: $username" };
+
+if ($result->entries != 1 ) { 
+ die "ERROR: User not found in AD: $username" 
+};
  
-my $entry = $result->entry(0); # there can be only one
+my $entry = $result->entry(0);
 my $dn = $entry->get_value('distinguishedName');
  
 my $unicodePwd = utf8(chr(34).${passwd}.chr(34))->utf16le();
@@ -60,3 +68,4 @@ $result->code && die $result->error;
 print "AD  : SUCCESS: ${username} password changed.n";
  
 $ad->unbind()
+
